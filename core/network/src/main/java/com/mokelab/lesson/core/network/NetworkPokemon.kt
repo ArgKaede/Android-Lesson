d9ae95008c91387d19a42a334ad7c2bb613793fe
ArgKaede <arg@mokelab.com>
2025-01-23 14:39:15 +0900

    fix

--- core/network/src/main/java/com/mokelab/lesson/core/network/NetworkPokemon.kt
+++ core/network/src/main/java/com/mokelab/lesson/core/network/NetworkPokemon.kt
@@ -1,9 +1,12 @@
 package com.mokelab.lesson.core.network
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class NetworkPokemon(
     val id: String,
     val name: String,
+
+    @SerialName("created") val createdAtMillis: Long,
 )
